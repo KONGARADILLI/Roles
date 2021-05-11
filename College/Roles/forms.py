76b8e868c597deb51a2da9b640d43ac0733dc50a
KONGARADILLI <dillikongara1681@gmail.com>
2021-05-11 12:52:38 +0530

    roles

--- College/Roles/forms.py
+++ College/Roles/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from Roles.models import User
 from django.contrib.auth.forms import UserCreationForm
 class RegForm(UserCreationForm):
+	password1= forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control my-2 ","placeholder":"Password"}))
+	password2= forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control my-2 ","placeholder":"confirm Password"}))
+
 	class Meta:
 		model= User
-		fields = '__all__'
+		fields = ["username"]
+		widgets = {
+		"username":forms.TextInput(attrs={"class":"form-control","placeholder":"Username"}),
+		}
--- College/Roles/views.py
+++ College/Roles/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from Roles.forms import RegForm
 # Create your views here.
 def homepage(request):
 	return render(request,'html/home.html')
 
 def register(request):
+	if request.method == "POST":
+		q = RegForm(request.POST)
+		if q.is_valid():
+			q.save()
+			return redirect('/lg')
 	q=RegForm()
-	return render(request,'html/register.html')
+	return render(request,'html/register.html',{'y':q})
